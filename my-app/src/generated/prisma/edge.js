e4f3a96e2bbd1a7cd3f36e68bae50c22d7c9b016
Pallab Midya <pallab.midya@floydee.com>
2025-05-07 12:30:35 +0530

    Create project

--- my-app/src/generated/prisma/edge.js
+++ my-app/src/generated/prisma/edge.js
@@ -122,7 +122,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
--- my-app/src/generated/prisma/index.js
+++ my-app/src/generated/prisma/index.js
@@ -123,7 +123,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
